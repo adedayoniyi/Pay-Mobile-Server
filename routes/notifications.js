89abed9410dfdb480a6738094987529c4db88604
martex-dev <adedayoniyio@gmail.com>
2023-05-12 16:12:13 +0100

    added notifications

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const Transactions = require("../models/transaction");
+const Notifications = require("../models/notifications_model");
+const auth = require("../middlewares/auth");
 
 const notificationsRouter = express.Router();
 
-notificationsRouter.get("/credit-notification/:username", async (req, res) => {
+notificationsRouter.get(
+  "/api/credit-notification/:username",
+  auth,
+  async (req, res) => {
+    try {
+      const { username } = req.params;
+      const transactions = await Transactions.find({
+        username: username,
+        trnxType: "Credit",
+      });
+      let showTransactionsFromRecentToLast = transactions.reverse();
+      let notifications = await Notifications.create({
+        username: showTransactionsFromRecentToLast[0].username,
+        trnxType: showTransactionsFromRecentToLast[0].trnxType,
+        amount: showTransactionsFromRecentToLast[0].amount,
+        sendersName:
+          showTransactionsFromRecentToLast[0].fullNameTransactionEntity,
+      });
+      notifications = await notifications.save();
+      res.status(200).json(notifications);
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  }
+);
+notificationsRouter.post("/api/deleteNotification", auth, async (req, res) => {
   try {
-    const { username } = req.params;
-    const transactions = await Transactions.find({
-      username: username,
-      trnxType: "Credit",
-    });
-    let showTransactionsFromRecentToLast = transactions.reverse();
-    res.json(showTransactionsFromRecentToLast);
+    const { username } = req.body;
+    await Notifications.findOneAndDelete({ username });
+    res.status(200).json({ message: "Deleted Successfully" });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
