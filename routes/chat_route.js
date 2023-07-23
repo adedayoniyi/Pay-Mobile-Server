22062836a37cae37210b7d51b886161ae9d5e8a8
martex-dev <adedayoniyio@gmail.com>
2023-07-23 18:18:05 +0100

    updated chat route

--- routes/chat_route.js
+++ routes/chat_route.js
@@ -2,10 +2,13 @@ const express = require("express");
 const Chat = require("../models/chat_model");
 const User = require("../models/user_model");
 const admin = require("../middlewares/admin_middleware");
+const Message = require("../models/message_model");
 const chatRouter = express.Router();
 
 chatRouter.post("/api/chat", async (req, res) => {
   const { sender, chatName } = req.body;
+  const initialMessageContent =
+    "Welcome to Pay Mobile customer support center, send us a message and we will reply as soon as possible";
   try {
     const agents = await User.find({ type: "agent" });
     if (agents.length === 0) {
@@ -26,16 +29,18 @@ chatRouter.post("/api/chat", async (req, res) => {
       });
       await newChat.save();
       const initialMessage = new Message({
-        sender: sender,
-        content:
-          "Welcome to Pay Mobile customer support center, send us a message and we will reply as soon as possible",
-        receiver: randomAgent.username,
+        sender: randomAgent.username,
+        content: initialMessageContent,
+        receiver: sender,
         chat: newChat._id,
       });
       await initialMessage.save();
-
+      await Chat.findByIdAndUpdate(chat._id, {
+        latestMessage: initialMessageContent,
+      });
       res.status(201).json(newChat);
     } else {
+      console.log("Chat Exists!!");
       res.status(200).json(chat);
     }
   } catch (error) {
